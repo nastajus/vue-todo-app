d20a510f9c901a6e2f0a4cfe85856371575bff87
Ian Nastajus <ian.nastajus@gmail.com>
2020-08-18 21:32:33 -0400

    clean

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,6 @@ export default {
                  //allowing <TodoThing...> to work in temp
   },
   //DATA IS A FUNCITON!!! NOT AN OBJECT!! 
-  //the "data key" refers to a function ... to communicate between components
-  //es6 supports any name
-  //"data" will return an object inside body of { }
   data  () { 
     return { 
       title2: "Ian's SECOND Sweet List"
