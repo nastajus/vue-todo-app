7a2a37c94f291a1128e41dcf2126fde0f40351cb
Ian Nastajus <ian.nastajus@gmail.com>
2020-08-24 19:41:02 -0400

    dark

--- src/App.vue
+++ src/App.vue
@@ -23,12 +23,16 @@ export default {
 </script>
 
 <style>
+body {
+  background: #2c3e50;
+  color: white;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center;  -- bullet fudges */ 
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
