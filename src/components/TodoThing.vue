68e5130d3316c28db0a2aa06a48ad515274ad59a
Ian Nastajus <ian.nastajus@gmail.com>
2020-08-24 19:07:08 -0400

    cleanup

--- src/components/TodoThing.vue
+++ src/components/TodoThing.vue
@@ -1,25 +1,15 @@
 <template>
-  <div class="todo-thing"> <!--formerly 'hello'-->
+  <div class="todo-thing"> 
     <h1>{{ title }}</h1> 
     <p>
       <ul>
-        <!--we don't want item to be key for performance reasons. -->
         <li v-for="(item, index) in items" :key="index">{{ item }}</li>
       </ul>
     </p>
     <input v-model="newItem" />
 
-    <!-- @ sign is for events ...--> 
-    <!-- () is optional / unnecessary for no parameters -->  
-    <!-- within vue ... not raw "button".. but augemented... instead it's bubbling up ... to  an emission event... (catching emission from component )--> 
-
-
     <button @click="addItem()" :disabled="!newItem" > Add Item </button>
 
-    <!-- ~~single component binding in vue2~~ -->
-    <!-- <button @click="deleteLast()" :disabled="!items.length"> Delete Last Item</button> -->
-
-    <!-- definitely not have () brackets... since it's exposed as a VALUE NOT A FUNCTION!! -->
     <button @click="deleteLast()" :disabled="deleteLastDisabled"> Delete Last Item</button>
   </div>
 </template>
@@ -27,16 +17,11 @@
 <script>
 export default {
   name: 'TodoThing',
-  //what this component expects to receive from PARENT COMPONENTS... 
 
-  //here in "props" we're `defining an interface`
   props: {
     title: String
   },
   
-  //here in "data" we're ... just assigning data ... 
-
-  // ~data~ is the "exception" that it "must" be a function...
   data () {
     return {
       items: [
@@ -50,26 +35,18 @@ export default {
   },
 
   computed: {
-    //vue checks.........the body... for references... smartly
     deleteLastDisabled () { 
       return !this.items.length
     }
   },
 
-  // most ~things~ are methods.. 
   methods: {
-    //javascript And Vue convention: SPACE for Defining Function/Meth 
     addItem () {
-      //vue ~maybe~ deviates from ~normal~ javascript this...
-      //~~any keys we create in data() are made available in methods()~~
-      
       this.items.push(this.newItem)
       this.newItem = "" 
-      //vue3 changes... something... with proxies... here...
     },
 
     deleteLast () {
-      // if (this.items.length <= 0) return;  //obselete
       this.items.splice(this.items.length-1)
     }
   }
